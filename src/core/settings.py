f63c6b61fb7e280a2c3c1003537b5a7d49b69e6e
Ali Soldatbay <aliardabekuly@gmail.com>
2024-05-01 22:36:54 +0600

    fix(): minor changes

--- src/core/settings.py
+++ src/core/settings.py
@@ -96,19 +96,19 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': env("POSTGRES_DB"),
-        'USER': env("POSTGRES_USER"),
-        'PASSWORD': env("POSTGRES_PASSWORD"),
-        'HOST': env("POSTGRES_HOST"),
-        'PORT': env("POSTGRES_PORT"),
-        "OPTIONS": {
-            'options': f'-c search_path={env("POSTGRES_SCHEMA")},public'
-        }
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': env("POSTGRES_DB"),
+#         'USER': env("POSTGRES_USER"),
+#         'PASSWORD': env("POSTGRES_PASSWORD"),
+#         'HOST': env("POSTGRES_HOST"),
+#         'PORT': env("POSTGRES_PORT"),
+#         "OPTIONS": {
+#             'options': f'-c search_path={env("POSTGRES_SCHEMA")},public'
+#         }
+#     }
+# }
 
 DATABASES = {
     'default': {
